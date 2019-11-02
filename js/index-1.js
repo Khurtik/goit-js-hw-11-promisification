22007ce92a4b7f386b02a5de90a017e9c8a789ad
sergiy khurtik <skhurtik@ukr.net>
2019-11-02 15:05:43 +0200

    hw-11 task-2

---
+++ js/index-1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    });
+  });
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+delay(1000).then(logger);
+delay(1500).then(logger);
+delay(2000).then(logger);
